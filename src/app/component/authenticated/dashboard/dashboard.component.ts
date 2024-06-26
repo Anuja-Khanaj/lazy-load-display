98544b1d7b9fdaaf8e74cfc56466088a45e18b33
Anuja Khanaj <anujahkhanaj2003@gmail.com>
2024-06-26 09:44:41 +0530

    dashboard added

--- src/app/component/authenticated/dashboard/dashboard.component.ts
+++ src/app/component/authenticated/dashboard/dashboard.component.ts
@@ -3,8 +3,9 @@ import {MatIconModule} from '@angular/material/icon';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AllProducts } from 'src/app/model/AllProducts';
 import { Product } from 'src/app/model/Product';
+import { AddTocartService } from 'src/app/service/add-tocart.service';
 import { ProductService } from 'src/app/service/product.service';
-
+import { WordPipe } from 'src/app/word.pipe';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -14,7 +15,9 @@ export class DashboardComponent {
   product: Product[] | undefined;
   products
 
-  constructor(private route: ActivatedRoute, private productService: ProductService, private router: Router) { }
+  constructor(private route: ActivatedRoute, private productService: ProductService,private cart:AddTocartService,private router:Router
+  ) {
+   }
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe((data) => {
@@ -24,5 +27,9 @@ export class DashboardComponent {
   }
   viewProduct(productId: number): void {
     this.router.navigate(['dashboard/view', productId]);
+  }
+  addtocart(data){
+    this.cart.save(data)
+    window.alert("Product added to cart")
   }
 }
